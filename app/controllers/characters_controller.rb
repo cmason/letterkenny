777aa903a927a5fa627cf0789bf1c8fe6d80453a
Chris Mason <chris@punknpoultry.com>
2022-04-07 23:10:49 -0500

    CUD of CRUD not yet needed

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -15,39 +15,39 @@ class CharactersController < ApplicationController
   def edit
   end
 
-  def create
-    @character = Quote.new(character_params)
+  # def create
+  #   @character = Quote.new(character_params)
 
-    respond_to do |format|
-      if @character.save
-        format.html { redirect_to @character, notice: "#{@character.name} was successfully created." }
-        format.json { render :show, status: :created, location: @character }
-      else
-        format.html { render :new }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @character.save
+  #       format.html { redirect_to @character, notice: "#{@character.name} was successfully created." }
+  #       format.json { render :show, status: :created, location: @character }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @character.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  def update
-    respond_to do |format|
-      if @character.update(character_params)
-        format.html { redirect_to @character, notice: "#{@character.name} was successfully updated." }
-        format.json { render :show, status: :ok, location: @character }
-      else
-        format.html { render :edit }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @character.update(character_params)
+  #       format.html { redirect_to @character, notice: "#{@character.name} was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @character }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @character.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  def destroy
-    @character.destroy
-    respond_to do |format|
-      format.html { redirect_to characters_url, notice: "Character was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @character.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to characters_url, notice: "Character was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,39 +15,39 @@ class QuotesController < ApplicationController
   def edit
   end
 
-  def create
-    @quote = Quote.new(quote_params)
+  # def create
+  #   @quote = Quote.new(quote_params)
 
-    respond_to do |format|
-      if @quote.save
-        format.html { redirect_to @quote, notice: "Quote was successfully created." }
-        format.json { render :show, status: :created, location: @quote }
-      else
-        format.html { render :new }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @quote.save
+  #       format.html { redirect_to @quote, notice: "Quote was successfully created." }
+  #       format.json { render :show, status: :created, location: @quote }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @quote.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  def update
-    respond_to do |format|
-      if @quote.update(quote_params)
-        format.html { redirect_to @quote, notice: "Quote was successfully updated." }
-        format.json { render :show, status: :ok, location: @quote }
-      else
-        format.html { render :edit }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @quote.update(quote_params)
+  #       format.html { redirect_to @quote, notice: "Quote was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @quote }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @quote.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  def destroy
-    @quote.destroy
-    respond_to do |format|
-      format.html { redirect_to quotes_url, notice: "Quote was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @quote.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to quotes_url, notice: "Quote was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
